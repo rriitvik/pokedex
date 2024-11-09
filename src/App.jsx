ddbaf6351605551762db05e992ba2829705b756c
Ritvik <gopalkaritvik117@gmail.com>
2024-11-09 10:54:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/Header'
+import SideNav from './components/SideNav'
+import PokeCard from './components/PokeCard'
+import { useState } from 'react'
+function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(0)
+  const [showSideMenu, setShowSideMenu] = useState(true)
+  function handleToggleMenu(){
+    setShowSideMenu(!showSideMenu)
+  }
+  function handleCloseMenu(){
+    setShowSideMenu(true)
+  }
+  return (
+    <>
+    <Header handleToggleMenu={handleToggleMenu}/>
+    <SideNav selectedPokemon={selectedPokemon} setSelectedPokemon={setSelectedPokemon} handleCloseMenu={handleCloseMenu} showSideMenu={showSideMenu}/>
+    <PokeCard selectedPokemon={selectedPokemon}/>
+    </>
+  )
+}
+
+export default App
